f3be7994935e4548472abeb748e7185b6198182e
Supsource <supriyomalstartup@gmail.com>
2023-05-11 10:41:16 +0530

    fix : fixed route setup

--- server/router/router.go
+++ server/router/router.go
@@ -1,12 +1,17 @@
 package router
 
 import (
-	"fmt"
-	"net/http"
+	"github.com/Supsource/todoapp-golang-react/middleware"
+	"github.com/gorilla/mux"
 )
 
-func Router() *http.ServeMux {
-	// implementation
-	fmt.Print("hello")
-	return http.NewServeMux()
+func Router() *mux.Router {
+	router := mux.NewRouter()
+	router.HandleFunc("/api/task", middleware.GetAllTasks).Methods("GET", "OPTIONS")
+	router.HandleFunc("/api/task", middleware.CreateTasks).Methods("GET", "OPTIONS")
+	router.handleFunc("/api/task/{id}", middleware.TaskComplete).Methods("PUT", "OPTIONS")
+	router.HandleFunc("/api/task/{id}", middleware.UndoTask).Methods("PUT", "OPTIONS")
+	router.HandleFunc("/api/task/{id}", middleware.DeleteTask).Methods("DELETE", "OPTIONS")
+	router.HandleFunc("/api/task/{id}", middleware.DeleteAllTask).Methods("DELETE", "OPTIONS")
+	return router
 }
