5199e9e7bcbd0b24e5bbb9d95a20cf550d667c39
Supsource <supriyomalstartup@gmail.com>
2023-05-22 19:44:21 +0530

    fix : fixed a typo

--- server/router/router.go
+++ server/router/router.go
@@ -9,7 +9,7 @@ func Router() *mux.Router {
 	router := mux.NewRouter()
 	router.HandleFunc("/api/task", middleware.GetAllTasks).Methods("GET", "OPTIONS")
 	router.HandleFunc("/api/task", middleware.CreateTasks).Methods("GET", "OPTIONS")
-	router.handleFunc("/api/task/{id}", middleware.TaskComplete).Methods("PUT", "OPTIONS")
+	router.HandleFunc("/api/task/{id}", middleware.TaskComplete).Methods("PUT", "OPTIONS")
 	router.HandleFunc("/api/task/{id}", middleware.UndoTask).Methods("PUT", "OPTIONS")
 	router.HandleFunc("/api/task/{id}", middleware.DeleteTask).Methods("DELETE", "OPTIONS")
 	router.HandleFunc("/api/task/{id}", middleware.DeleteAllTask).Methods("DELETE", "OPTIONS")
